d1c546663e2d343e2ff97159749ccc36280c7d07
prahal <vprahaladhan@gmail.com>
2021-06-25 11:57:30 +1000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,5 @@
 module ArtistsHelper
+  def display_artist(song)
+    (song.artist_name.nil?) ? ["Add Artist", "/songs/#{song.id}/edit"] : ["#{song.artist_name}", "/artists/#{song.artist.id}"]
+  end
 end
